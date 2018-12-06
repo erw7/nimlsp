73e1a052584e3c68da49464607e362ae4b3d671a
erw7 <erw7.github@gmail.com>
2018-12-06 22:05:30 +0900

    Change to use forth instead of name

--- src/nimlsppkg/mappings.nim
+++ src/nimlsppkg/mappings.nim
@@ -19,21 +19,21 @@ func nimSymToLSPKind(suggest: Suggest): CompletionItemKind =
 
 func nimSymDetails(suggest: Suggest): string =
   case suggest.symkind.TSymKind:
-  of skConst: "const " & suggest.qualifiedPath[0] & ": " & cast[string](suggest.name)
-  of skEnumField: "enum " & cast[string](suggest.name)
-  of skForVar: "for var of " & cast[string](suggest.name)
-  of skIterator: cast[string](suggest.name)
+  of skConst: "const " & (if suggest.qualifiedPath.len != 0: suggest.qualifiedPath.join(".") else: "") & ": " & suggest.forth
+  of skEnumField: "enum " & suggest.forth
+  of skForVar: "for var of " & suggest.forth
+  of skIterator: suggest.forth
   of skLabel: "label"
-  of skLet: "let of " & cast[string](suggest.name)
+  of skLet: "let of " & suggest.forth
   of skMacro: "macro"
-  of skMethod: cast[string](suggest.name)
+  of skMethod: suggest.forth
   of skParam: "param"
-  of skProc: cast[string](suggest.name)
+  of skProc: suggest.forth
   of skResult: "result"
-  of skTemplate: cast[string](suggest.name)
+  of skTemplate: suggest.forth
   of skType: "type " & suggest.qualifiedPath[0]
-  of skVar: "var of " & cast[string](suggest.name)
-  else: cast[string](suggest.name)
+  of skVar: "var of " & suggest.forth
+  else: suggest.forth
 
 func nimDocstring(suggest: Suggest): string =
   suggest.doc.unescape()
